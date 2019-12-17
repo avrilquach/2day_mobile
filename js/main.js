6be7db5ad4a724004db27c198b61e2feadafc100
avril quach <flaxnguyen@gmail.com>
2019-12-17 09:23:20 -0700

    upfile video detail

--- js/main.js
+++ js/main.js
@@ -75,45 +75,16 @@ $(document).ready(function() {
 	$('.form-box-search a.cancelSearch').click(function(e){
 		$('.form-box-search').hide();
 	});
-	var customer = $('.main-customer');
-	customer.owlCarousel({
-			loop: true,
-			nav: true,
-			navText: ['<i class="fa fa-arrow-left" aria-hidden="true"></i>', '<i class="fa fa-arrow-right" aria-hidden="true"></i>'],
-			items: 1,
-			autoplay: true,
-			smartSpeed: 200,
-			responsiveClass:true,
-				responsive:{
-						0:{
-								items:1
-						},
-						768:{
-								items:1
-						},
-						1200:{
-								items:1
-						}
-				}
-	});
-	var customer = $('.main-bang-gia');
-	customer.owlCarousel({
+	var banner = $('.main-banner');
+	banner.owlCarousel({
 			loop: true,
 			nav: false,
+			dots: true,
 			items: 1,
+			nav: false,
 			autoplay: true,
-			smartSpeed: 200,
-			responsiveClass:true,
-				responsive:{
-						0:{
-								items:1
-						},
-						768:{
-								items:1
-						},
-						1200:{
-								items:1
-						}
-				}
+			animateOut: 'fadeOut',
+			animateIn: 'fadeInLeft',
+			smartSpeed: 200
 	});
 });
